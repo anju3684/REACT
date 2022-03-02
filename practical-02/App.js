5563f95dbb303a61c34d6ed3740e376f14d64f61
Anjali Shah <anjushah2507@gmail.com>
2022-03-02 15:11:27 +0530

    added different file of webpack

---
+++ practical-02/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import "./index.css";
+class App extends Component{
+   render(){
+      return(
+         <div className='center'>
+            <h1>Hello Anjali</h1>
+         </div>
+      );
+   }
+}
+export default App;
--- practical-02/webpack.config.js
+++ practical-02/webpack.config.js
@@ -1,30 +1,6 @@
-const { rules } = require('eslint-config-prettier');
-const path=require('path');
-module.exports = {
-    mode: 'development',
-    entry: './index.js',
-    output: {
-      path: path.resolve(__dirname, 'public'),
-      filename: 'main.js',
-    },
-  target:"node",
-  devServer:{
-      port:"3001",
-      //contentBase:["./public"],
-      open:true
-  },
-  resolve:{
-    extensions:[".js",".jsx",".json"]
-  },
-  module:{
-      rules:[
-          {
-            test: /\.jsx?$/,
-            exclude: /node_modules/,
-            loader: 'babel-loader',
-          }
-
-      ]
-  },
-  
-}
+const { merge } = require("webpack-merge");
+const commonWebpackConfig = require("./webpack.common.js");
+module.exports = ({ env }) => {
+  const envWebpackConfig = require(`./webpack.${env}.js`);
+  return merge(commonWebpackConfig, envWebpackConfig);
+};
